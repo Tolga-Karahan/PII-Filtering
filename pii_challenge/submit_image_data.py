1b1e2901355697e627bb8602806a094d1f71b034
Tolga <tolga.karahan@hypatos.ai>
2024-10-10 21:22:47 +0300

    refactor the code

--- pii_challenge/submit_image_data.py
+++ pii_challenge/submit_image_data.py
@@ -3,6 +3,7 @@ import json
 import os
 from pathlib import Path
 import pika
+from pika.adapters.blocking_connection import BlockingChannel
 import logging
 
 
@@ -17,6 +18,50 @@ def connect_to_broker():
     return connection
 
 
+def submit_images(channel):
+    supported_image_extensions = os.getenv(
+        "SUPPORTED_IMAGE_EXTENSIONS", "jpg,png"
+    ).split(",")
+    logging.info(f"Supported extensions: {supported_image_extensions}")
+
+    image_folder = Path(os.getenv("IMAGE_SOURCE")).joinpath("images")
+    metadata_folder = Path(os.getenv("IMAGE_SOURCE")).joinpath("metadata")
+    images = os.listdir(image_folder)
+
+    logging.debug(images)
+    for img in images:
+        publish_image_data(img, supported_image_extensions, metadata_folder, channel)
+
+
+def publish_image_data(
+    img: str, supported_image_extensions: list[str], metadata_folder_path: Path, channel: BlockingChannel
+):
+    logging.debug(img)
+    path = Path(img)
+    extension = path.suffix
+    logging.info(extension)
+
+    if extension in supported_image_extensions:
+        name = path.stem
+        terms_file = f"{name}.json"
+
+        terms_file_path = metadata_folder_path.joinpath(terms_file)
+        logging.info(f"terms_file_path: {terms_file_path}")
+
+        if terms_file in os.listdir(metadata_folder_path):
+            logging.info(f"Sending the message for {img}!")
+            with open(terms_file_path) as f:
+                pii_terms = json.load(f)["pii_terms"]
+                logging.debug(pii_terms)
+
+            # queue_declare is idempotent. Result is same no matter how many times it is called.
+            channel.queue_declare("images")
+            body = {"image_path": str(img), "pii_terms": pii_terms}
+            channel.basic_publish(
+                exchange="", routing_key="images", body=json.dumps(body)
+            )
+
+
 if __name__ == "__main__":
     try:
         logging.basicConfig(
@@ -25,45 +70,7 @@ if __name__ == "__main__":
         dotenv.load_dotenv()
         con = connect_to_broker()
         channel = con.channel()
-
-        supported_image_extensions = os.getenv(
-            "SUPPORTED_IMAGE_EXTENSIONS", "jpg,png"
-        ).split(",")
-        logging.info(f"Supported extensions: {supported_image_extensions}")
-
-        image_folder = Path(os.getenv("IMAGE_SOURCE")).joinpath("images")
-        metadata_folder = Path(os.getenv("IMAGE_SOURCE")).joinpath("metadata")
-        images = os.listdir(image_folder)
-
-        logging.debug(images)
-        for img in images:
-            logging.debug(img)
-            path = Path(img)
-            extension = path.suffix
-            logging.info(extension)
-
-            if extension in supported_image_extensions:
-                name = path.stem
-                terms_file = f"{name}.json"
-
-                image_path = image_folder.joinpath(img)
-                logging.info(f"path: {image_path}")
-
-                terms_file_path = metadata_folder.joinpath(terms_file)
-                logging.info(f"terms_file_path: {terms_file_path}")
-
-                if terms_file in os.listdir(metadata_folder):
-                    logging.info(f"Sending the message for {img}!")
-                    with open(terms_file_path) as f:
-                        pii_terms = json.load(f)["pii_terms"]
-                        logging.debug(pii_terms)
-
-                    # queue_declare is idempotent. Result is same no matter how many times it is called.
-                    channel.queue_declare("images")
-                    body = {"image_path": str(img), "pii_terms": pii_terms}
-                    channel.basic_publish(
-                        exchange="", routing_key="images", body=json.dumps(body)
-                    )
+        submit_images(channel)
 
     except Exception as e:
         logging.error(e)
