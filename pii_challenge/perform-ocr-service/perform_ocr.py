eec200f7b5f4640918ef033d0a741e93cfcbd99d
Tolga <tolga.karahan@hypatos.ai>
2024-10-10 18:39:57 +0300

    format with black

--- pii_challenge/perform-ocr-service/perform_ocr.py
+++ pii_challenge/perform-ocr-service/perform_ocr.py
@@ -1,45 +1,46 @@
 from PIL import Image
-from PIL.ImageFile import ImageFile
 import json
 import logging
 import os
 
 import dotenv
+from pathlib import Path
 import pika
 import pytesseract
 from pytesseract import Output
-import pandas as pd
 
 from models.models import TextBoundingBox
 from utils.utils import EnhancedJSONEncoder
+
 
 def connect_to_broker():
     credentials = pika.PlainCredentials(
         os.getenv("RABBITMQ_USERNAME"), os.getenv("RABBITMQ_PASSWORD")
     )
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(credentials=credentials)
+    parameters = pika.ConnectionParameters(
+        host=os.getenv("RABBITMQ_HOST"), port=5672, credentials=credentials
     )
+    connection = pika.BlockingConnection(parameters)
     logging.info(f"Connection is open: {connection.is_open}")
     return connection
 
+
 def callback(ch, method, properties, body):
     body = json.loads(body.decode())
-    logging.info(f"Consumed message: {body}")
+    logging.debug(f"Consumed message: {body}")
     img_path = body["image_path"]
-    
-    boxes = detect_text(img_path)
+    abs_image_path = (
+        Path(os.getenv("DATA_SOURCE")).joinpath("images").joinpath(img_path)
+    )
+    boxes = detect_text(abs_image_path)
     # ch.basic_ack()
-    
+
     # publish bounding boxes
     logging.info(f"Publishing bounding boxes!")
-    # queue_declare is idempotent. Result is same no matter how many times it is called.
-    ch.queue_declare("bounding_boxes")
     body["boxes"] = json.dumps(boxes, cls=EnhancedJSONEncoder)
-    ch.basic_publish(
-        exchange="", routing_key="bounding_boxes", body=json.dumps(body)
-    )
-        
+    ch.basic_publish(exchange="", routing_key="bounding_boxes", body=json.dumps(body))
+
+
 def detect_text(img_path: str) -> list[TextBoundingBox]:
     result = pytesseract.image_to_data(
         Image.open(img_path),
@@ -50,17 +51,17 @@ def detect_text(img_path: str) -> list[TextBoundingBox]:
         TextBoundingBox(
             text=result["text"][i],
             left=result["left"][i],
-            right=result["left"][i]+result["width"][i],
+            right=result["left"][i] + result["width"][i],
             top=result["top"][i],
-            bottom=result["top"][i]+result["height"][i],
-        ) 
+            bottom=result["top"][i] + result["height"][i],
+        )
         for i in range(len(result["level"]))
-        if result["conf"][i] > 0.5 and result["text"][i].strip() != ''
+        if result["conf"][i] > 0.5 and result["text"][i].strip() != ""
     ]
     logging.info("Boxes are calculated!")
-    
+
     return boxes
-    
+
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -69,5 +70,8 @@ if __name__ == "__main__":
     dotenv.load_dotenv()
     con = connect_to_broker()
     channel = con.channel()
+    # queue_declare is idempotent. Result is same no matter how many times it is called.
+    channel.queue_declare("bounding_boxes")
+    channel.queue_declare("images")
     channel.basic_consume(queue="images", auto_ack=True, on_message_callback=callback)
     channel.start_consuming()
--- pii_challenge/submit_image_data.py
+++ pii_challenge/submit_image_data.py
@@ -17,8 +17,6 @@ def connect_to_broker():
     return connection
 
 
-
-
 if __name__ == "__main__":
     try:
         logging.basicConfig(
@@ -27,7 +25,7 @@ if __name__ == "__main__":
         dotenv.load_dotenv()
         con = connect_to_broker()
         channel = con.channel()
-        
+
         supported_image_extensions = os.getenv(
             "SUPPORTED_IMAGE_EXTENSIONS", "jpg,png"
         ).split(",")
@@ -36,33 +34,33 @@ if __name__ == "__main__":
         image_folder = Path(os.getenv("IMAGE_SOURCE")).joinpath("images")
         metadata_folder = Path(os.getenv("IMAGE_SOURCE")).joinpath("metadata")
         images = os.listdir(image_folder)
-        
+
         logging.debug(images)
         for img in images:
             logging.debug(img)
             path = Path(img)
             extension = path.suffix
             logging.info(extension)
-            
+
             if extension in supported_image_extensions:
                 name = path.stem
                 terms_file = f"{name}.json"
-                
+
                 image_path = image_folder.joinpath(img)
                 logging.info(f"path: {image_path}")
-                
+
                 terms_file_path = metadata_folder.joinpath(terms_file)
                 logging.info(f"terms_file_path: {terms_file_path}")
-                
+
                 if terms_file in os.listdir(metadata_folder):
                     logging.info(f"Sending the message for {img}!")
                     with open(terms_file_path) as f:
                         pii_terms = json.load(f)["pii_terms"]
                         logging.debug(pii_terms)
-                    
+
                     # queue_declare is idempotent. Result is same no matter how many times it is called.
                     channel.queue_declare("images")
-                    body = {"image_path": str(image_path), "pii_terms": pii_terms}
+                    body = {"image_path": str(img), "pii_terms": pii_terms}
                     channel.basic_publish(
                         exchange="", routing_key="images", body=json.dumps(body)
                     )
